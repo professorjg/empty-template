b4ae2416cfaa370ab6066af6923a61e9048f0e16
professor <113101211+professorjg@users.noreply.github.com>
2023-02-11 02:07:43 -0500

    empty

---
+++ src/main/java/edu/wit/cs/comp1050/EX1a.java
@@ -0,0 +1,3 @@
+package edu.wit.cs.comp1050;
+
+// TODO: write the rest of the code
--- src/test/java/edu/wit/cs/comp1050/tests/TestSuite.java
+++
@@ -1,7 +0,0 @@
-package edu.wit.cs.comp1050.tests;
-
-public class TestSuite {
-	static String stringOutput(String[] lines, Object[] values) {
-		return String.format(String.join("", lines), values);
-	}
-}
